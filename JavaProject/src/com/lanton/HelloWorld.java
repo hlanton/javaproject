c13b028e75f13b5fea703c8ebbcf7bba8a4ae990
Administrator <Administrator@WIN-SBP0QK8FM3A>
2020-05-03 16:18:50 +0800

    lanton

--- JavaProject/src/com/lanton/HelloWorld.java
+++ JavaProject/src/com/lanton/HelloWorld.java
@@ -2,7 +2,7 @@ package com.lanton;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("欢迎来到java世界,对这个文件进行一定得到修改操作");
+		System.out.println("欢迎来到java世界,对这个文件进行一定的修改操作");
 	}
 
 }
